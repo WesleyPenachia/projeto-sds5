9051866535e6ee2b255d754e6f78ca2052e26307
Wesley Penachia <wesleyikiko@hotmail.com>
2021-11-02 17:00:39 -0300

    Basic Static Components

---
+++ frontend/src/components/Footer/index.tsx
@@ -0,0 +1,15 @@
+const Footer = () => {
+    return (
+        <div>
+            <footer className="footer mt-auto py-3 bg-dark">
+                <div className="container">
+                    <p className="text-light">App desenvolvido por <a href="https://github.com/WesleyPenachia" target="_blank" rel="noreferrer">Wesley Penachia</a></p>
+                    <p className="text-light"><small><strong>Semana Spring React</strong><br />
+                        Evento promovido pela escola DevSuperior: <a href="https://instagram.com/WesleyPenachia" target="_blank" rel="noreferrer">@wesleypenachia</a></small></p>
+                </div>
+            </footer>
+        </div>
+    );
+}
+
+export default Footer;
